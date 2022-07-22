ee33709bfaeb03ac7de92c0cd3966cccd0e7564e
nothors2 <nothorse@gmail.com>
2022-07-22 14:48:41 +0900

    docker test 1

--- src/main/java/com/webAdmin/controller/RestSample.java
+++ src/main/java/com/webAdmin/controller/RestSample.java
@@ -38,7 +38,7 @@ public class RestSample {
         int sec = (int) (500 * dValue);
         logger.error("RestSample Log sec : "+sec);
         Map<String,Object> map = new HashMap<>();
-        map.put("home","testtesttesttesttest docker v1 1");
+        map.put("home",". docker v22222");
         map.put("dValue",""+sec);
         try {
             Thread.sleep(sec);
